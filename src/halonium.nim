451696762cd498ce510c8190deda19ff88ffbc0f
Joey Yakimowich-Payne <jyapayne@gmail.com>
2020-03-09 21:14:52 -0600

    Cleanup

--- src/halonium.nim
+++ src/halonium.nim
@@ -1,60 +1,9 @@
-import options
-import haloniumpkg/commands
 import haloniumpkg/exceptions
-import haloniumpkg/service
-import haloniumpkg/utils
 import haloniumpkg/webdriver
 import haloniumpkg/browser
 import haloniumpkg/driveroptions
 
-export commands
 export exceptions
-export service
-export utils
 export webdriver
 export browser
 export driveroptions
-
-proc main() =
-  var session = createSession(Chrome, browserOptions=chromeOptions(args=["--headless"]))
-
-  session.navigate("http://demo.guru99.com/test/upload/")
-  let element = session.waitForElement("#uploadfile_0").get()
-
-  element.uploadFile("/Users/joey/Downloads/Question-Checklist.pdf")
-
-  session.actionChain()
-         .click("#terms")
-         .click("#submitbutton")
-         .perform()
-  # session.safariSetPermission("getUserMedia", false)
-  # echo session.safariPermissions["getUserMedia"]
-  # echo session.executeScript("return [1,2,3,4,5];")
-  # echo session.allCookies()
-  # echo session.newWindow(WindowKind.Window).kind
-  # var rect = session.currentWindow.rect
-  # rect.height = 1000;
-  # session.currentWindow.rect = rect
-  # echo session.currentWindow.size
-  # session.currentWindow.maximize
-
-  # discard session.actionChain()
-  #                .sendKeys("#search-box", "hey", Key.Enter)
-  #                .click(".post-main .post-title .thread-title a")
-  #                .perform(debugMouseMove=true)
-
-  # echo session.log(ltBrowser)
-  # echo session.log(ltDriver)
-  # let elements = session.findElements(".thread-title")
-  # for element in elements:
-  #   echo element.attribute("class")
-  #   echo element.property("innerText")
-  #   echo element.rect
-
-  # while true:
-  #   try:
-  #     sleep(1000)
-  #   except:
-  #     session.stop()
-  #     break
-main()
--- src/haloniumpkg/webdriver.nim
+++ src/haloniumpkg/webdriver.nim
@@ -1421,64 +1421,6 @@ proc clearActions*(chain: ActionChain): ActionChain =
     chain.session.clearActions()
   chain
 
-
-const DEBUG_MOUSE_MOVE_SCRIPT = """
-  var id = "haloniumMouseDebugging";
-  var dotID = "haloniumMouseDebuggingDot";
-  var descID = "haloniumMouseDebuggingDescription";
-  var element = arguments[0];
-  var x = arguments[1];
-  var y = arguments[2];
-  var rect = element.getBoundingClientRect();
-
-  var el, redDot, description;
-  if (document.getElementById(id) == null) {
-    el = document.createElement("div");
-    redDot = document.createElement("div");
-    description = document.createElement("div");
-    el.appendChild(redDot);
-    el.appendChild(description);
-
-    el.id = id;
-    redDot.id = dotID;
-    description.id = descID;
-
-    el.style.position = "absolute";
-    el.style.zIndex = "100000000";
-    el.style.display = "flex";
-    el.style.pointerEvents = "none";
-
-    redDot.style.borderRadius = "5px";
-    redDot.style.border = "2px solid red";
-    redDot.style.backgroundColor = "red";
-    redDot.style.width = "5px";
-    redDot.style.height = "5px";
-    redDot.style.display = "inline-block";
-    redDot.style.pointerEvents = "none";
-    redDot.style.marginRight = "5px";
-
-    description.style.display = "inline-block";
-    description.style.border = "1px solid black";
-    description.style.backgroundColor = "white";
-    description.style.borderRadius = "3px";
-    description.style.pointerEvents = "none";
-    description.style.paddingLeft = "5px";
-    description.style.paddingRight = "5px";
-
-    document.body.appendChild(el);
-  } else {
-    el = document.getElementById(id);
-    redDot = document.getElementById(dotID);
-    description = document.getElementById(descID);
-  }
-  el.style.top = (rect.top + y) + "px";
-  el.style.left = (rect.left + x) + "px";
-  description.innerHTML = "Moved to (x: " + el.style.left + ", y: " + el.style.top + ")";
-  console.log(x);
-  console.log(y);
-  console.log(element);
-"""
-
 proc actionToJson(chain: ActionChain, action: Action, debugMouseMove = false): JsonNode =
   let isW3C = chain.session.w3c
   case action.ty
@@ -1585,7 +1527,7 @@ proc actionToJson(chain: ActionChain, action: Action, debugMouseMove = false): J
             "element": action.origin.elementId,
           }
 
-proc createNewW3CActions*(
+proc createNewW3CActions(
   chain: ActionChain,
   keyActions: seq[Action],
   pointerActions: seq[Action],
@@ -1665,7 +1607,7 @@ proc `%`*(element: Element): JsonNode =
     "element-6066-11e4-a52e-4f735466cecf": element.id
   }
 
-proc execute(element: Element, command: Command, params: JsonNode = %*{}): JsonNode =
+proc execute*(element: Element, command: Command, params: JsonNode = %*{}): JsonNode =
   var newParams = params
   newParams["elementId"] = %element.id
   newParams["sessionId"] = %element.session.id
