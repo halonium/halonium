4593f2bfa580812c92c4ed5b5b0ec2b3c0f3ecf6
Joey Yakimowich-Payne <jyapayne@gmail.com>
2020-05-10 16:46:41 -0600

    Add simple example

---
+++ examples/simple.nim
@@ -0,0 +1,19 @@
+import options, os
+import halonium
+
+proc main() =
+  var session = createSession(Chrome)
+  session.navigate("https://google.com")
+
+  let searchBar = "input[title=\"Search\"]"
+  let element = session.waitForElement(searchBar).get()
+
+  element.sendKeys("clowns", Key.Enter)
+
+  let firstATag = session.waitForElement("#search a").get()
+  firstATag.click()
+  sleep(1000)
+
+  session.stop()
+
+main()
